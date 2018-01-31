d903e0dda35fb53da100b2da31cd9c35a2c61e4a
nishun0419 <ninosekunlight@gmail.com>
2018-01-31 17:21:55 +0900

    Modified slightly

--- php/help.php
+++ php/help.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="/php/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/php/css/help.css">
-	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="/php/css/navbar.css">
 	<script type="text/javascript" src="/php/js/jquery-3.1.1.min.js"></script>
 	<script type="text/javascript" src="/php/js/bootstrap.min.js"></script>
 	<title>ヘルプ</title>
