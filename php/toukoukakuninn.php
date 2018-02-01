f428219eb7ef86984248b16a4f4859a1aa76c305
nishun0419 <ninosekunlight@gmail.com>
2018-02-01 09:38:52 +0900

    toukoucomp fix

--- php/toukoukakuninn.php
+++ php/toukoukakuninn.php
@@ -7,6 +7,7 @@
 <link rel="stylesheet" type="text/css" href="/php/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="/php/css/navbar.css">
 <link rel="stylesheet" type="text/css" href="/php/css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="/php/css/toukou.css">
 <script type="text/javascript" src="/php/js/jquery-3.1.1.min.js"></script>
 <script type="text/javascript" src="/php/js/jquery-ui.min.js"></script>
 <script type="text/javascript" src="/php/js/dispCalendar.js"></script>
